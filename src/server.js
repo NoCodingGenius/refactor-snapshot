4e3a63400ba1f19ba7c2a807957fa7d5566501f2
LaToya Williams <latoyawilliams@LaToyas-MacBook-Pro.local>
2018-01-29 15:56:53 -0800

    Initial setup and installations. Move the public directory to the root of the project.

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ const passport = require('passport');
 
 server.set('view engine', 'ejs');
 server.set('views', __dirname + '/views');
-server.use(express.static(__dirname + '/public'));
+server.use(express.static('public'));
 
 server.use(session({
   store: new pgSession({
